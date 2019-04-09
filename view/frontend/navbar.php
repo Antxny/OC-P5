9a24701258d17bdff567077d3ff351031029f97d
Antony <antony.v94@outlook.com>
2019-04-09 07:00:23 +0200

    User - Fix navbar login / register
    Edit :
    • navbar.php : Adding a section for login / register

--- view/frontend/navbar.php
+++ view/frontend/navbar.php
@@ -7,4 +7,32 @@
 		<a href="index.php?action=listPosts" class="<?php if ($page == 'blog'){ echo 'active'; } ?>">blog</a>
 		<a  class="<?php if ($page == 'portfolio'){ echo 'active'; } ?>" style="cursor: not-allowed">portfolio</a>
 	</nav>
+	<div class="sign">
+		
+		<?php if (isset($_SESSION['id'])): ?>
+
+		<div class="btn-group">
+            <button type="button" class="btn dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><?= $_SESSION['username'] ?></button>
+            <div class="dropdown-menu dropdown-menu-right">
+               	<a class="dropdown-item nav-my-profile" href="#">Mon profil</a>
+               	<div class="dropdown-divider"></div>
+
+               	<?php if ($_SESSION['rank'] == 2): ?>
+               	<a class="dropdown-item nav-manage-comments" href="index.php?action=manageComments">Gestion des commentaires</a>
+               	<a class="dropdown-item nav-logs" href="index.php?action=logs">Afficher les logs</a>
+               	<div class="dropdown-divider"></div>
+               	<?php endif ?>
+
+                <a class="dropdown-item nav-logout" href="index.php?action=logout">Déconnexion</a>
+            </div>
+        </div>
+
+		<?php else : ?>
+			
+			<a href="index.php?action=register" class="btn-signup">S'inscrire</a>
+			<a href="index.php?action=login" class="btn-signin">Se connecter</a>
+			
+		<?php endif ?>
+
+	</div>
 </div>
